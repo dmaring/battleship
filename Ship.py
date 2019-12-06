acba89702136b78a5ee1230d72f6be5d14e6976f
Drew Maring <andrew.maring@gmail.com>
2019-12-06 14:28:49 -0800

    Change direction argument to an Enum

--- Ship.py
+++ Ship.py
@@ -1,25 +1,31 @@
 """
 Ship class for battleship game
 """
+from enum import Enum
+
+
+class Direction(Enum):
+    vertical = 1
+    horizontal = 2
 
 
 class Ship:
 
-    def __init__(self, x: int, y: int, direction: str, length: int):
+    def __init__(self, x: int, y: int, direction: Direction, length: int):
         # direction True = vertical, direction False = horizontal
         self.coords = []
-        if direction.lower() == 'vert':
+        if direction == Direction.vertical:
             for i in range(length):
                 self.coords.append((x, y + i))
-        elif direction.lower() == 'hor':
+        else:
             for i in range(length):
-                self.coords.append(tuple([x + i, y]))
+                self.coords.append((x + i, y))
         self.health = length
 
     def print_coords(self):
         print(self.coords)
 
-    def dec_health(self, amount=1: int):
+    def dec_health(self, amount=1):
         if self.health:
             self.health -= 1
         else:

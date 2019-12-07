c3d590160c567d2da8e62e60d657b4cab07c6cdd
Tu <tuvo1106@gmail.com>
2019-12-06 17:42:49 -0800

    Change player representation on board

--- Game.py
+++ Game.py
@@ -12,17 +12,17 @@ class Board:
             # for y in range(len(self.screen)):
             print(self.screen[x])
 
-    def fill_board(self):
+    def fill_board(self, symbol):
         for ship in self.fleet:
             for x, y in ship.coords:
                 if self.validate_placement(x, y) == False:
                     print("not a valid placement")
                     break
                 else:
-                    self.screen[x][y] = 'S'
+                    self.screen[x][y] = symbol
 
     def validate_hit(self, x, y):
-        if self.screen[x][y] == 'S':
+        if self.screen[x][y] != 0:
             for ship in self.fleet[:]:
                 for coord in ship.coords:
                     if coord == (x, y):
@@ -36,11 +36,8 @@ class Board:
                         return True
         return False
 
-
-    def add_ship(self, x, y, direction, length):
+    def add_ship(self, x, y, direction, length, player):
         print("adding ship(s)...")
-        # s = Ship(0, 1, Direction.vertical, 2)
-        # s2 = Ship(0, 2, Direction.horizontal, 3)
         ship_dict = {
             "1": Destroyer,
             "2": Submarine,
@@ -50,13 +47,19 @@ class Board:
         ship_const = ship_dict.get(length)
         self.fleet.append(ship_const(int(x), int(y), direction))
         self.ships_remaining += 1
-        self.fill_board()
+        symbol = ''
+        if player == 1:
+            symbol = "1"
+        else:
+            symbol = "2"
+        self.fill_board(symbol)
         self.display_board()
 
     def validate_placement(self, x, y):
         if self.screen[x][y] != 0:
             return False
         return True
+
 
 class Player:
     def __init__(self, player_num: int):
@@ -68,25 +71,25 @@ class Game:
     def __init__(self):
         self.p1 = Player(1)
         self.p2 = Player(2)
-        self.current_player = self.p2
-        self.opponent = self.p1
+        self.current_player = self.p1
+        self.opponent = self.p2
 
     def start_game(self):
         if self.current_player.board.ships_remaining == 0:
-            self.initialize_ships()
+            self.initialize_ships(1)
             self.toggle_turn()
-            self.initialize_ships()
+            self.initialize_ships(2)
         game_on = True
         while game_on:
             self.toggle_turn()
             x, y = self.get_user_input()
             hit = self.opponent.board.validate_hit(int(x), int(y))
-                
             if hit and self.check_victory():
                 game_on = False
             self.display_score()
 
     def get_user_input(self):
+        print()
         print("Current player: ***" + self.current_player.name + "***")
         x = y = ""
         while not x.isdigit():
@@ -106,8 +109,12 @@ class Game:
         if self.opponent.board.ships_remaining == 0:
             return True
 
-    def initialize_ships(self):
+    def initialize_ships(self, player: int):
         # print("Player 1, how many ships would you like to start off with?")
+        print("======================")
+        print("NEW GAME")
+        print("======================")
+        print(f"{self.current_player.name}")
         x = input("Give x coordinate: ")
         y = input("Give y coordinate: ")
         d = input("Which direction? (v or h): ")
@@ -116,10 +123,9 @@ class Game:
             direction = Direction.vertical
         elif (d == "h"):
             direction = Direction.horizontal
-        l = input("what type of ship? 1=destroyer 2=submarine 3=battleship 4=carrier")
-        self.current_player.board.add_ship(x, y, direction, l)
-        # if ship_type == 4:
-
+        l = input(
+            "What type of ship?\n1) Destroyer\n2) Submarine\n3) Battleship \n4) Carrier\n")
+        self.current_player.board.add_ship(x, y, direction, l, player)
 
     def display_score(self):
         print("\n======================")
@@ -127,8 +133,6 @@ class Game:
         print(f"Player 1 has {self.p1.board.ships_remaining} remaining ships.")
         print(f"Player 2 has {self.p2.board.ships_remaining} remaining ships.")
         print("======================\n")
-
-
 
 
 g = Game()
